3b54a9841135a796ed5e1845fa53ee51add8dcab
Kai <kai@pazdzewicz.de>
2018-08-09 16:59:42 +0200

    updated login site

--- gui/app/forms/LoginForm.php
+++ gui/app/forms/LoginForm.php
@@ -13,61 +13,48 @@ use Phalcon\Validation\Validator\Identical;
 
 class LoginForm extends Form
 {
-
-    public function initialize()
-    {
-        // Email
-        $email = new Text('email', [
-            'placeholder' => 'Email'
-        ]);
-
-        $email->addValidators([
-            new PresenceOf([
-                'message' => 'The e-mail is required'
-            ]),
-            new Email([
-                'message' => 'The e-mail is not valid'
-            ])
-        ]);
-
-        $this->add($email);
-
-        // Password
-        $password = new Password('password', [
-            'placeholder' => 'Password'
-        ]);
-
-        $password->addValidator(new PresenceOf([
-            'message' => 'The password is required'
-        ]));
-
-        $password->clear();
-
-        $this->add($password);
-
-        // Remember
-        $remember = new Check('remember', [
-            'value' => 'yes'
-        ]);
-
-        $remember->setLabel('Remember me');
-
-        $this->add($remember);
-
-        // CSRF
-        $csrf = new Hidden('csrf');
-
-        $csrf->addValidator(new Identical([
-            'value' => $this->security->getSessionToken(),
-            'message' => 'CSRF validation failed'
-        ]));
-
-        $csrf->clear();
-
-        $this->add($csrf);
-
-        $this->add(new Submit('go', [
-            'class' => 'btn btn-success'
-        ]));
-    }
+  public function initialize()
+  {
+    // Email
+    $email = new Text('email', [
+      'placeholder' => 'Email',
+      'class' => 'form-control'
+    ]);
+    $email->addValidators([
+      new PresenceOf([
+        'message' => 'The e-mail is required'
+      ]),
+      new Email([
+        'message' => 'The e-mail is not valid'
+      ])
+    ]);
+    $this->add($email);
+    $password = new Password('password', [
+      'placeholder' => 'Password',
+      'class' => 'form-control'
+    ]);
+    $password->addValidator(new PresenceOf([
+      'message' => 'The password is required'
+    ]));
+    $password->clear();
+    $this->add($password);
+    // Remember
+    $remember = new Check('remember', [
+      'value' => 'yes'
+    ]);
+    $remember->setLabel('Remember me');
+    $this->add($remember);
+    // CSRF
+    $csrf = new Hidden('csrf');
+    $csrf->addValidator(new Identical([
+      'value' => $this->security->getSessionToken(),
+      'message' => 'CSRF validation failed'
+    ]));
+    $csrf->clear();
+    $this->add($csrf);
+    $this->add(new Submit('go', [
+      'class' => 'btn btn-success form-control',
+      'value' => 'Login'
+    ]));
+  }
 }
--- gui/app/views/session/login.volt
+++ gui/app/views/session/login.volt
@@ -1,30 +1,51 @@
-{{ content() }}
-
-<div align="center" class="well">
-
-	{{ form('class': 'form-search') }}
-
-	<div align="left">
-		<h2>Log In</h2>
+<div class="row">
+	<div class="col-lg-12">
+		{{ content() }}
 	</div>
-
-		{{ form.render('email') }}
-		{{ form.render('password') }}
-		{{ form.render('go') }}
-
-		<div align="center" class="remember">
-			{{ form.render('remember') }}
-			{{ form.label('remember') }}
-		</div>
-
-		{{ form.render('csrf', ['value': security.getToken()]) }}
-
-		<hr>
-
-		<div class="forgot">
-			{{ link_to("session/forgotPassword", "Forgot my password") }}
-		</div>
-
-	</form>
-
 </div>
+
+<div class="row">
+	<div class="col-lg-12">
+		<h1>Login</h1>
+	</div>
+</div>
+
+{{ form('class': 'form-search') }}
+	{{ form.render('csrf', ['value': security.getToken()]) }}
+	<div class="row">
+		<div class="col-lg-6">
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text">E-Mail</span>
+				</div>
+				{{ form.render('email') }}
+			</div>
+		</div>
+		<div class="col-lg-6">
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text">Password</span>
+				</div>
+				{{ form.render('password') }}
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-3">
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<div class="input-group-text">
+						{{ form.render('remember') }}
+					</div>
+				</div>
+				<p class="form-control">{{ form.label('remember') }}</p>
+			</div>
+		</div>
+		<div class="col-lg-3">
+			{{ link_to("session/forgotPassword", "Forgot my password", 'class':'form-control btn btn-light') }}
+		</div>
+		<div class="col-lg-6">
+			{{ form.render('go') }}
+		</div>
+	</div>
+</form>
