b7c8f06e495f0954d940b5067668a02d3fc14357
Kai <kai@pazdzewicz.de>
2018-08-18 18:41:10 +0200

    started working on peppermint dns service

--- gui/app/controllers/ControllerBase.php
+++ gui/app/controllers/ControllerBase.php
@@ -12,56 +12,47 @@ use Phalcon\Mvc\Dispatcher;
  */
 class ControllerBase extends Controller
 {
-    /**
-     * Execute before the router so we can determine if this is a private controller, and must be authenticated, or a
-     * public controller that is open to all.
-     *
-     * @param Dispatcher $dispatcher
-     * @return boolean
-     */
-    public function beforeExecuteRoute(Dispatcher $dispatcher)
-    {
-        $controllerName = $dispatcher->getControllerName();
-
-        // Only check permissions on private controllers
-        if ($this->acl->isPrivate($controllerName)) {
-
-            // Get the current identity
-            $identity = $this->auth->getIdentity();
-            $this->view->identity = $identity;
-
-            // If there is no identity available the user is redirected to index/index
-            if (!is_array($identity)) {
-
-                $this->flash->notice('You don\'t have access to this module: private');
-
-                $dispatcher->forward([
-                    'controller' => 'index',
-                    'action' => 'index'
-                ]);
-                return false;
-            }
-
-            // Check if the user have permission to the current option
-            $actionName = $dispatcher->getActionName();
-            if (!$this->acl->isAllowed($identity['profile'], $controllerName, $actionName)) {
-
-                $this->flash->notice('You don\'t have access to this module: ' . $controllerName . ':' . $actionName);
-
-                if ($this->acl->isAllowed($identity['profile'], $controllerName, 'index')) {
-                    $dispatcher->forward([
-                        'controller' => $controllerName,
-                        'action' => 'index'
-                    ]);
-                } else {
-                    $dispatcher->forward([
-                        'controller' => 'user_control',
-                        'action' => 'index'
-                    ]);
-                }
-
-                return false;
-            }
+  /**
+  * Execute before the router so we can determine if this is a private controller, and must be authenticated, or a
+  * public controller that is open to all.
+  *
+  * @param Dispatcher $dispatcher
+  * @return boolean
+  */
+  public function beforeExecuteRoute(Dispatcher $dispatcher)
+  {
+    $controllerName = $dispatcher->getControllerName();
+    // Only check permissions on private controllers
+    if ($this->acl->isPrivate($controllerName)) {
+      // Get the current identity
+      $identity = $this->auth->getIdentity();
+      $this->view->identity = $identity;
+      // If there is no identity available the user is redirected to index/index
+      if (!is_array($identity)) {
+        $this->flash->notice('You don\'t have access to this module: private');
+        $dispatcher->forward([
+          'controller' => 'index',
+          'action' => 'index'
+        ]);
+        return false;
+      }
+      // Check if the user have permission to the current option
+      $actionName = $dispatcher->getActionName();
+      if (!$this->acl->isAllowed($identity['profile'], $controllerName, $actionName)) {
+        $this->flash->notice('You don\'t have access to this module: ' . $controllerName . ':' . $actionName);
+        if ($this->acl->isAllowed($identity['profile'], $controllerName, 'index')) {
+          $dispatcher->forward([
+            'controller' => $controllerName,
+            'action' => 'index'
+          ]);
+        } else {
+          $dispatcher->forward([
+            'controller' => 'user_control',
+            'action' => 'index'
+          ]);
         }
+        return false;
+      }
     }
+  }
 }
--- gui/app/views/index/index.volt
+++ gui/app/views/index/index.volt
@@ -2,12 +2,32 @@
 	<div class="col-lg-12">
 		<header class="jumbotron subhead" id="overview">
 			<div class="hero-unit">
-				<h1>Welcome!</h1>
-				<p class="lead">This is your PowerDNS GUI.</p>
-				<div align="right">
-		      {{ link_to('session/login', '<i class="fas fa-sign-in-alt"></i> Login', 'class': 'btn btn-success btn-large') }}
+				<h1>Welcome to Peppermint.Cloud DNS Service!</h1>
+				<p class="lead">Your premium DNS Service. Without costs!</p>
+				<div class="row">
+					<div class="col-lg-6">
+						{{ link_to('session/signup', '<i class="fas fa-user-plus"></i> Signup for Free', 'class': 'btn btn-primary btn-large form-control') }}
+					</div>
+					<div class="col-lg-6">
+						{{ link_to('session/login', '<i class="fas fa-sign-in-alt"></i> Login', 'class': 'btn btn-success btn-large form-control') }}
+					</div>
 				</div>
 			</div>
 		</header>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-lg-4">
+		<h2>Free!</h2>
+		<p>Our service is free to use. No Credit Card or PayPal required for signup or for using the service.</p>
+	</div>
+	<div class="col-lg-4">
+		<h2>Reliable</h2>
+		<p>We use a high availability mysql cluster, local database syncs on our nameserver (for failover, if the main mysql cluster dies, never happened). PowerDNS is the choice of software to use in our infrastructure, it is fast and lightweight.</p>
+	</div>
+	<div class="col-lg-4">
+		<h2>Non-Profit</h2>
+		<p>We operate on non-profit base and therefore we need donations to keep our services live. {{ link_to('donate','Maybe you want to donate some bucks?') }}</p>
 	</div>
 </div>
